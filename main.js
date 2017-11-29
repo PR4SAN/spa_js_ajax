f0d54d1e2de28278430d8790f5b6a38f35070edf
Suprasanna Ojha <ssojha95@gmail.com>
2017-11-30 01:10:22 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-function test() {
-  document.getElementById('view').innerHTML = 'test success';
-}
-
-function getPage(page) {
+function getPage(page, title, url) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById('view').innerHTML = this.responseText;
-      console.log(this.responseText);
+      var obj = { Title: title, Url: url };
+      history.pushState(obj, obj.Title, obj.Url);
     }
   };
   xhr.open('GET', page, true);

855f334fe06518d48c30c14d903ef42face4828a
Suprasanna <iamsojha@bitbucket.org>
2017-12-01 11:33:50 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
-function getPage(page, title, url) {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById('view').innerHTML = this.responseText;
-      var obj = { Title: title, Url: url };
-      history.pushState(obj, obj.Title, obj.Url);
-    }
-  };
-  xhr.open('GET', page, true);
-  xhr.send();
+function supports_history_api() {
+  return !!(window.history && history.pushState);
 }
+
+function swapPhoto(href) {
+  var req = new XMLHttpRequest();
+  req.open("GET",
+           "http://127.0.0.1:5500/pages/" +
+             href.split("/").pop(),
+           false);
+  req.send(null);
+  if (req.status == 200) {
+    document.getElementById("view").innerHTML = req.responseText;
+    setupHistoryClicks();
+    return true;
+  }
+  return false;
+}
+
+function addClicker(link) {
+  link.addEventListener("click", function(e) {
+    if (swapPhoto(link.href)) {
+      history.pushState(null, null, link.href);
+      e.preventDefault();
+    }
+  }, false);
+}
+
+function setupHistoryClicks() {
+  addClicker(document.getElementById("home"));
+  addClicker(document.getElementById("about"));
+  addClicker(document.getElementById("contact"));
+  
+}
+
+window.onload = function() {
+  if (!supports_history_api()) { return; }
+  setupHistoryClicks();
+  window.setTimeout(function() {
+    window.addEventListener("popstate", function(e) {
+      swapPhoto(location.pathname);
+    }, false);
+  }, 1);
+};
